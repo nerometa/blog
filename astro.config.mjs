aeca16655b5429a84b221d1f55ad0d2578f1e52c
nerometa <nerometa@protonmail.com>
2024-02-18 22:28:40 +0700

    build: change site for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "http://localhost:4321",
+	site: "https://nerometa.github.io",
 	integrations: [
 		mdx(),
 		icon({
