55b1e683e05a61e3e902d926fe62539fa32a031e
nerometa <nerometa@protonmail.com>
2024-10-03 16:47:50 +0700

    new blog styling and added i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,4 +38,8 @@ export default defineConfig({
 	devToolbar: {
 		enabled: false,
 	},
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en', 'th']
+	}
 });
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,4 @@
+export type Lang = "en" | "th" | string;
+
+export const defaultLocale: string = "en";
+export const locales = ["en", "th"];
