fba122d08673b6947bd2622fbff9f4ccb2365434
nerometa <nerometa@protonmail.com>
2026-04-10 04:24:09 +0000

    fix: change content path

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod"
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/blog" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./content/blog" }),
   schema: z.object({
     title: z.string(),
     pubDate: z
