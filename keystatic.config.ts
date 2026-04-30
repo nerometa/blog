8acd01448945d8e4f1adc01bb8d22922c77de928
nerometa <nerometa@protonmail.com>
2026-04-30 10:35:53 +0000

    fix(cms): change to github mode

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,8 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: "nerometa/github",
   },
   collections: {
     posts: collection({
