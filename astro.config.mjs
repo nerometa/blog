87fab07f7ac781f1e29b56b41e8f0c039e505552
nerometa <nerometa@protonmail.com>
2026-04-30 10:34:00 +0000

    feat(cms): add keystatic cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,47 +2,55 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.nerometa.dev/",
-	integrations: [
-		mdx(),
-		icon({
-			svgoOptions: {
-				multipass: true,
-				plugins: [
-					{
-						name: "preset-default",
-						params: {
-							overrides: {
-								// customize default plugin options
-								inlineStyles: {
-									onlyMatchedOnce: false,
-								},
+  site: "https://blog.nerometa.dev/",
+  integrations: [
+    mdx(),
+    icon({
+      svgoOptions: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                // customize default plugin options
+                inlineStyles: {
+                  onlyMatchedOnce: false,
+                },
 
-								// or disable plugins
-								removeDoctype: false,
-							},
-						},
-					},
-				],
-			},
-		}),
-	],
-	markdown: {
-		shikiConfig: {
-			theme: "rose-pine-moon",
-			wrap: true,
-		},
-	},
-	devToolbar: {
-		enabled: false,
-	},
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en', 'th']
-	},
-	image: {
-		responsiveImages: true,
-	}
+                // or disable plugins
+                removeDoctype: false,
+              },
+            },
+          },
+        ],
+      },
+    }),
+    react(),
+    markdoc(),
+    keystatic(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "rose-pine-moon",
+      wrap: true,
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "th"],
+  },
+  image: {
+    responsiveImages: true,
+  },
 });
+
---
+++ keystatic.config.ts
@@ -0,0 +1,37 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/blog/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.mdx({ label: "Content" }),
+        pubDate: fields.date({
+          label: "Published Date",
+        }),
+        draft: fields.checkbox({
+          label: "Draft",
+          defaultValue: true,
+        }),
+        description: fields.text({
+          label: "Description",
+        }),
+        tags: fields.array(fields.text({ label: "Tag" }), {
+          label: "Tag",
+          itemLabel: (props) => props.value,
+        }),
+        author: field.ignored({
+          label: "Author",
+          defaultValue: "nerometa",
+        }),
+      },
+    }),
+  },
+});
