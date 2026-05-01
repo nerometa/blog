63ae04ac985f0140ad468141783d145ddaff15d5
nerometa <nerometa@protonmail.com>
2026-05-01 23:49:55 +0700

    fix: remove `slug`

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,17 @@
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
-import { z } from "astro/zod"
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    draft: z.boolean().default(false),
-    description: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    author: z.string(),
-    slug: z.string(),
-  }),
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        draft: z.boolean().default(false),
+        description: z.string().optional(),
+        tags: z.array(z.string()).optional(),
+        author: z.string(),
+    }),
 });
 
 export const collections = { blog };
