f5733a2b7aaae5d2dc29fec200a31b26ffca9801
nerometa <nerometa@protonmail.com>
2026-05-01 23:35:08 +0700

    fix(cms): fix repo name

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,38 +1,38 @@
 import { config, fields, collection } from "@keystatic/core";
 
 export default config({
-  storage: {
-    kind: "github",
-    repo: "nerometa/github",
-  },
-  collections: {
-    posts: collection({
-      label: "Posts",
-      slugField: "title",
-      path: "src/content/blog/*",
-      format: { contentField: "content" },
-      schema: {
-        title: fields.slug({ name: { label: "Title" } }),
-        content: fields.mdx({ label: "Content" }),
-        pubDate: fields.date({
-          label: "Published Date",
+    storage: {
+        kind: "github",
+        repo: "nerometa/blog",
+    },
+    collections: {
+        posts: collection({
+            label: "Posts",
+            slugField: "title",
+            path: "src/content/blog/*",
+            format: { contentField: "content" },
+            schema: {
+                title: fields.slug({ name: { label: "Title" } }),
+                content: fields.mdx({ label: "Content" }),
+                pubDate: fields.date({
+                    label: "Published Date",
+                }),
+                draft: fields.checkbox({
+                    label: "Draft",
+                    defaultValue: true,
+                }),
+                description: fields.text({
+                    label: "Description",
+                }),
+                tags: fields.array(fields.text({ label: "Tag" }), {
+                    label: "Tag",
+                    itemLabel: (props) => props.value,
+                }),
+                author: fields.text({
+                    label: "Author",
+                    defaultValue: "nerometa",
+                }),
+            },
         }),
-        draft: fields.checkbox({
-          label: "Draft",
-          defaultValue: true,
-        }),
-        description: fields.text({
-          label: "Description",
-        }),
-        tags: fields.array(fields.text({ label: "Tag" }), {
-          label: "Tag",
-          itemLabel: (props) => props.value,
-        }),
-        author: fields.text({
-          label: "Author",
-          defaultValue: "nerometa",
-        }),
-      },
-    }),
-  },
+    },
 });
